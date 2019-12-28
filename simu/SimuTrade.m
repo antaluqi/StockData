a63d0afff02b52e2c0ea1ef8ecf29e8312bac6c5
antaluqi <33272769+antaluqi@users.noreply.github.com>
2019-12-28 09:48:23 +0800

    常规添加模块

--- simu/SimuTrade.m
+++ simu/SimuTrade.m
@@ -2,54 +2,105 @@ classdef SimuTrade<handle
    properties
        Code % 代码
        data    % 数据源
-       sDate % 购买开始日
-       sPrice % 购买价格
-       bPrice % 基价
-       cPrice % 成本价
-       amount % 持有量
-       sl   % stoploss 止损率
-       tp   % takeprofit 止盈率
-       add  % 加仓率
+       sDate  % 初始购买日
+       sPrice % 初始购买单价
+       cPrice % 当前单价
+       cDate  % 当前日期
+       nDate % 下个日期
+       sAmount % 初始购买数量
+       cAmount % 当前数量
        tLog % 交易日志
        
    end
    methods
-       function obj=SimuTrade(Code,data,sDate,sPrice,sl,tp,add)  % 构造函数 
+       function obj=SimuTrade(Code,data,sDate,sPrice,sAmount)  % 构造函数 
            %addpath('..\stock');
            obj.Code=Code;
            obj.data=data;
            obj.sDate=sDate;
            obj.sPrice=sPrice;
-           obj.bPrice=sPrice;
            obj.cPrice=sPrice;
-           obj.amount=0;
-           obj.sl=sl;
-           obj.tp=tp;
-           obj.add=add;
-           
+           obj.cDate=sDate;
+           obj.sAmount=sAmount;
+           obj.cAmount=sAmount;
        end
 
        function set.data(obj,value)
-           if ismember('Data',fieldnames(value)) && ismember('Open',fieldnames(value)) && ismember('High',fieldnames(value))&& ismember('Low',fieldnames(value))&& ismember('Close',fieldnames(value)) && ismember('Volume',fieldnames(value))
+           if ismember('Date',fieldnames(value)) && ismember('Open',fieldnames(value)) && ismember('High',fieldnames(value))&& ismember('Low',fieldnames(value))&& ismember('Close',fieldnames(value)) && ismember('Volume',fieldnames(value))
               obj.data=value;
            else
               error('data必须要包含 Date\Open\High\Low\Close\Volume字段')
            end
        end
        
-       function Start(obj)
-           dL=obj.data(obj.data.Date>obj.sDate,:);
-           obj.amount=1;
-           for i=1:height(dL)
-              if (obj.data.High(i)-obj.cPrice)/obj.cPrice>=obj.tp
-              end
-              if (obj.data.Low(i)-obj.cPrice)/obj.cPrice<=obj.sl
-              end
-              if (obj.data.Low(i)-obj.bPrice)/obj.bPrice<=obj.add
-              end
+       function set.sDate(obj,value)
+           if any(obj.data.Date==value)
+               obj.sDate=value;
+           else
+               error('开始日期sPrice超出数据源data的日期范围之外')
            end
        end
-   end
+       
+       function set.cDate(obj,value)
+           if any(obj.data.Date==value)
+               obj.cDate=value;
+               obj.nDate=obj.NextDate(value);
+           else
+               error('当前日期cPrice超出数据源data的日期范围之外')
+           end           
+       end
+       
+       function set.sPrice(obj,value)
+           i=find(obj.data.Date==obj.sDate);
+           if value<=obj.data.High(i) && value>=obj.data.Low(i)
+               obj.sPrice=value;
+           else
+              error([obj.Code,'的sPrice数据',num2str(value),'在',num2str(obj.sDate),'日超出当日数据范围[',num2str(obj.data.Low(i)),',',num2str(obj.data.High(i)),']']);
+           end
+       end
+       
+       function value=NextDate(obj,cdate)
+           if find(obj.data.Date==cdate)>=height(obj.data)
+               value='';
+           else
+               value=obj.data(find(obj.data.Date==cdate)+1,:).Date;
+           end
+       end
+       
+       function out=StepNext(obj)
+           if isempty(obj.nDate)
+               obj.EndFun
+               out=0;
+           else
+               SL_Check(obj);
+               TP_Check(obj)
+               Add_Check(obj)
+               Sub_Check(obj)
+               obj.cDate=obj.nDate;
+               out=1;
+           end
+       end
+       
+       function SL_Check(obj) % 止损检测
+           disp('止损检测')
+       end
+       
+       function TP_Check(obj) % 止盈检测
+           disp('止盈检测')
+       end
+       
+       function Add_Check(obj) % 加仓检测
+           disp('加仓检测')
+       end
+       
+       function Sub_Check(obj) % 减仓检测
+           disp('减仓检测')
+       end
+       
+       function EndFun(obj) % 结束处理函数
+           disp('结束')
+       end
+   end     
    methods (Access='private')
    end
    methods (Static) 
